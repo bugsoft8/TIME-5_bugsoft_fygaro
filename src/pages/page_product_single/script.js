470232d3cb72764099b58bb666dccc4adc3a6e83
gustavo <gustavoronn@gmail.com>
2023-06-23 20:08:55 -0300

    feat: checked_single_product

--- src/pages/page_product_single/script.js
+++ src/pages/page_product_single/script.js
@@ -1,10 +1,17 @@
+const bolinhas = document.getElementsByClassName ('bolinha');
+
 const $checkbox = document.querySelector('#switch');
 
 $checkbox.addEventListener('change', function(){
     if($checkbox.checked==true){
         document.getElementById("imagem").src="../../assets/images/product_single.svg";
+
+        bolinhas[0].classList.remove('bolinha_verde')
+        bolinhas[1].classList.add('bolinha_verde')
     }
     else{
         document.getElementById("imagem").src="../../assets/images/product_single.svg";
+        bolinhas[1].classList.remove('bolinha_verde')
+        bolinhas[0].classList.add('bolinha_verde')
     }
 });
