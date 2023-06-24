8a4e670e55b1ee049453a65a5bcfe1d75433442f
gustavo <gustavoronn@gmail.com>
2023-06-24 01:44:27 -0300

    product_single almost done

--- src/pages/page_product_single/script.js
+++ src/pages/page_product_single/script.js
@@ -1,5 +1,12 @@
 const bolinhas = document.getElementsByClassName ('bolinha');
 
+const $payment = document.getElementsByClassName ('btn_payment');
+
+$payment.addEventListener('click', function(){
+    
+});
+
+
 const $checkbox = document.querySelector('#switch');
 
 $checkbox.addEventListener('change', function(){
@@ -10,7 +17,7 @@ $checkbox.addEventListener('change', function(){
         bolinhas[1].classList.add('bolinha_verde')
     }
     else{
-        document.getElementById("imagem").src="../../assets/images/img.product_single_2.svg";
+        document.getElementById("imagem").src="../../assets/images/product_single--2.svg";
         bolinhas[1].classList.remove('bolinha_verde')
         bolinhas[0].classList.add('bolinha_verde')
     }
